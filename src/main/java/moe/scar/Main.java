b727090b221cb8113354b2a34926dba0cc7a5abd
scar <scarletmacaw73@gmail.com>
2025-09-11 21:27:32 -0400

    Added renderer code

--- src/main/java/moe/scar/Main.java
+++ src/main/java/moe/scar/Main.java
@@ -1,67 +1,74 @@
 package moe.scar;
 
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.TerminalFactory;
 import com.googlecode.lanterna.*;
 import java.io.IOException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/>
+import static moe.scar.Renderer.*;
+
 public class Main {
-    static Terminal terminal;
 
-    static {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        var terminal = new DefaultTerminalFactory().createTerminal();
+        var screen = new TerminalScreen(terminal);
+
         try {
-            terminal = new DefaultTerminalFactory().createTerminal();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public Main() throws IOException {
-    }
-
-    public static void main(String[] args) throws IOException {
-            TerminalSize size = terminal.getTerminalSize();
-            borders(size);
+            screen.startScreen();
+            screen.setCursorPosition(null);
+            TerminalSize size = screen.getTerminalSize();
+            TextGraphics textGraphics = screen.newTextGraphics();
             while (true) {
-                KeyStroke key;
-                key = terminal.readInput();
-                if (key.getKeyType() == KeyType.ArrowDown) {
-                    terminal.clearScreen();
-                    render();
+                KeyStroke key = screen.pollInput();
+                TerminalSize newSize = screen.doResizeIfNecessary();
+                if (newSize != null) {
+                    screen.clear();
+                    borders(screen, newSize);
+                    render(screen, newSize, textGraphics);
                 }
+                if (key != null) {
+                    if (key.getKeyType() == KeyType.ArrowDown) {
+                        screen.clear();
+                        render(screen, screen.getTerminalSize(), textGraphics);
+                    } else if (key.getKeyType() == KeyType.ArrowUp) {
+                        break; // exit
+                    }
+                }
+                render(screen, screen.getTerminalSize(), textGraphics);
+                Thread.sleep(100);
             }
+        } finally {
+            screen.stopScreen();
+        }
+
+    }
+    public static void render(Screen screen, TerminalSize size, TextGraphics tg) throws IOException {
+
+        borders(screen, size);
+
+        //deck  list list to content seperator
+        drawVerticalLine(size, screen, 2, size.getColumns()/3, size.getRows()-3, Symbols.DOUBLE_LINE_VERTICAL);
+
+        //top bar seperator
+
+        drawHorizontalLine(size, screen, 0, 2, size.getColumns(), Symbols.DOUBLE_LINE_HORIZONTAL);
+
+        tg.putString(2, 1, "Moxfield Tui");
+
+        tg.putString(size.getColumns()-9, 1, "v1.0.0");
+
+        //account info seperator
+        drawHorizontalLine(size, screen, 0,size.getRows()-10,size.getColumns()/3, Symbols.DOUBLE_LINE_HORIZONTAL);
+
+        screen.refresh();
     }
 
-    public static void render() throws IOException {
-        TerminalSize size = terminal.getTerminalSize();
-        borders(size);
-//        terminal.setCursorPosition(1, ((size.getColumns() / 3)));
-        for (int i = 0; i < size.getColumns(); i++){
-            terminal.putCharacter('|');
-            terminal.setCursorPosition(((size.getColumns() / 3)), i);
-            terminal.flush();
-        }
     }
-    public static void borders(TerminalSize size) throws IOException {
-        terminal.setCursorPosition(0,0);
-        for (int i = 1; i < size.getColumns()-1; i++){
-            terminal.setCursorPosition(i,0);
-            terminal.putCharacter('-');
-            terminal.setCursorPosition(i, size.getColumns()-1);
-            terminal.putCharacter('-');
-            terminal.flush();
-        }
-        for (int i = 0; i < size.getRows(); i++){
-            terminal.setCursorPosition(0,i);
-            terminal.putCharacter('|');
-            terminal.setCursorPosition(size.getColumns()-1, i);
-            terminal.putCharacter('|');
-            terminal.flush();
-        }
-    }
-}
---
+++ src/main/java/moe/scar/Renderer.java
@@ -0,0 +1,70 @@
+package moe.scar;
+
+import com.googlecode.lanterna.Symbols;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+
+import java.io.IOException;
+
+public class Renderer {
+
+
+
+
+    public static void drawVerticalDivider(TerminalSize size, Screen screen, int x, int rows) {
+        drawVerticalLine(size, screen, 0, x, rows, Symbols.DOUBLE_LINE_VERTICAL);
+
+//        screen.setCharacter(x, 0, new TextCharacter(Symbols.DOUBLE_LINE_T_DOWN));
+//        screen.setCharacter(x, rows - 1, new TextCharacter(Symbols.DOUBLE_LINE_T_UP));
+    }
+
+    public static void drawVerticalLine(TerminalSize size, Screen screen, int yStart, int x, int length, char symbol) {
+        for (int y = yStart; y < yStart + length; y++) {
+            screen.setCharacter(x, y, new TextCharacter(symbol));
+            if (y == 0){
+                screen.setCharacter(x, y, new TextCharacter(Symbols.DOUBLE_LINE_T_DOWN));
+            }
+            if (y == size.getRows()- 1){
+                screen.setCharacter(x, y, new TextCharacter(Symbols.DOUBLE_LINE_T_UP));
+            }
+        }
+    }
+
+    public static void drawHorizontalLine(TerminalSize size, Screen screen, int xStart, int y, int length, char symbol) {
+        for (int x = xStart; x < xStart + length; x++) {
+            screen.setCharacter(x, y, new TextCharacter(symbol));
+
+            if (x == 0){
+                screen.setCharacter(x, y, new TextCharacter(Symbols.DOUBLE_LINE_T_RIGHT));
+            }
+            if (x == size.getColumns()- 1){
+                screen.setCharacter(x, y, new TextCharacter(Symbols.DOUBLE_LINE_T_LEFT));
+            }
+        }
+
+    }
+
+    public static void borders(Screen screen, TerminalSize size) throws IOException {
+
+        for (int i = 0; i <= size.getColumns(); i++) {
+            screen.setCharacter(i, 0, new TextCharacter(Symbols.DOUBLE_LINE_HORIZONTAL));
+            screen.setCharacter(i, size.getRows()-1, new TextCharacter(Symbols.DOUBLE_LINE_HORIZONTAL));
+
+        }
+
+        for (int i = 0; i < size.getRows(); i++) {
+            screen.setCharacter(0, i, new TextCharacter(Symbols.DOUBLE_LINE_VERTICAL));
+            screen.setCharacter(size.getColumns() - 1, i, new TextCharacter(Symbols.DOUBLE_LINE_VERTICAL));
+        }
+
+        screen.setCharacter(0,0, new TextCharacter(Symbols.DOUBLE_LINE_TOP_LEFT_CORNER));
+        screen.setCharacter(0,size.getRows() - 1, new TextCharacter(Symbols.DOUBLE_LINE_BOTTOM_LEFT_CORNER));
+        screen.setCharacter(size.getColumns()-1,0, new TextCharacter(Symbols.DOUBLE_LINE_TOP_RIGHT_CORNER));
+        screen.setCharacter(size.getColumns()-1,size.getRows() - 1, new TextCharacter(Symbols.DOUBLE_LINE_BOTTOM_RIGHT_CORNER));
+
+        screen.refresh();
+    }
+
+
+}
